af47a0f8c84d2c09987c8b23da65cbf47e05f18e
stack07142 <stack.07142@gmail.com>
2018-08-09 13:14:37 +0900

    BehavirSubject - 백버튼 두번 눌러 종료하기

---
+++ app/src/main/java/io/github/stack07142/rxjava_android_samples/MainActivity.kt
@@ -0,0 +1,50 @@
+package io.github.stack07142.rxjava_android_samples
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
+import io.github.stack07142.rxjava_android_samples.fragments.MainFragment
+import io.reactivex.BackpressureStrategy
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.subjects.BehaviorSubject
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+    private val backButtonBehaviorSubject = BehaviorSubject.createDefault(0L).toSerialized()
+    private var disposable: Disposable? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (savedInstanceState == null) {
+            fragmentManager
+                    .beginTransaction()
+                    .replace(android.R.id.content, MainFragment(), this.toString())
+                    .commit()
+        }
+
+        disposable = backButtonBehaviorSubject.toFlowable(BackpressureStrategy.BUFFER)
+                .observeOn(AndroidSchedulers.mainThread())
+                .buffer(2, 1)
+                .map { it[0] to it[1] }
+                .subscribe {
+                    Timber.d("first= ${it.first}, second= ${it.second}")
+                    if (it.second - it.first < 1500) {
+                        finish()
+                    } else {
+                        Toast.makeText(this, "종료할까요?", Toast.LENGTH_SHORT).show()
+                    }
+                }
+    }
+
+    override fun onBackPressed() {
+        backButtonBehaviorSubject.onNext(System.currentTimeMillis())
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        disposable?.dispose()
+    }
+}
